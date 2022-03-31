981f6727fdad29b787347a34a89e2cf262e52c4b
Mizanur Rahman <mizanur.rahman@navana.com>
2022-03-31 15:38:50 +0600

    network section done with mongo db

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose")
 
 const app = express();
+
+mongoose.connect("mongodb://mizan:mizan@mongo:27017/?authSource=admin")
+.then(() => console.log("Succesfully connected to DB")).catch(e => console.log(e))
 
 app.get("/", (req, res) => {
   res.send("<h2>Hi theredsdff</h1>");
