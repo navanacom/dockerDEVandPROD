3754dad31aa7f15ebccb58db8265901b9226ea4e
Mizanur Rahman <mizanur.rahman@navana.com>
2022-03-31 16:38:32 +0600

    redis installed with -V

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP } = require("./config/config");
 
 const app = express();
 
-mongoose.connect("mongodb://mizan:mizan@mongo:27017/?authSource=admin")
-.then(() => console.log("Succesfully connected to DB")).catch(e => console.log(e))
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}/?authSource=admin`
+
+const connectWithRetry = () => {
+  mongoose.connect(mongoUrl)
+  .then(() => console.log("Succesfully connected to DB")).catch(e => {
+    console.log(e)
+    setTimeout(connectWithRetry, 5000)
+  })
+  
+}
+
+connectWithRetry()
 
 app.get("/", (req, res) => {
   res.send("<h2>Hi theredsdff</h1>");
