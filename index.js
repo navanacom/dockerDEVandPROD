6b130e1996e1b2274e5a729c8c75c7ce227958bd
Mizanur Rahman <mizanur.rahman@navana.com>
2022-03-31 14:23:42 +0600

    before mongo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h2>Hi theredf</h1>");
+  res.send("<h2>Hi theredsdff</h1>");
 });
 
 const port = process.env.PORT || 3000;
